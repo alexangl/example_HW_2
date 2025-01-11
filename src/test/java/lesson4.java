3f455e732e39c537ffe2f2cd5ad1900c49b627d5
alexangl <alexangl@mail.ru>
2025-01-11 23:09:01 +0300

    Update lesson4.java

--- src/test/java/lesson4.java
+++ src/test/java/lesson4.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
 
-public class MyLesson4 {
+public class lesson4 {
     
    @Test
    void testProjectLesson4() {
