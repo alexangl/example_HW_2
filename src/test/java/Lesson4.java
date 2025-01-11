bee3432c74644ea661bf8f180df9e8c09e62d8db
alexangl <alexangl@mail.ru>
2025-01-11 23:34:10 +0300

    Lesson4 final commit

--- src/test/java/Lesson4.java
+++ src/test/java/Lesson4.java
@@ -1,26 +1,23 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class Lesson4 {
-    
-   @Test
-   void testProjectLesson4() {
-     
-    open("https://github.com");
-    $(".search-input").click();
-    $("#query-builder-test").setValue("selenide").pressEnter();
-    $("a[href='/selenide/selenide']").click();
-    $("#repository-container-header").shouldHave(text("selenide / selenide"));
-    $("#wiki-tab").click();
-    $("#wiki-pages-filter").setValue("SoftAssertions");
-    $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
-    $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-    $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+    @Test
+    void testProjectLesson4() {
+        open("https://github.com");
+        $(".search-input").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        $("a[href='/selenide/selenide']").click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
     }
 }
