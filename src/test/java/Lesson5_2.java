0518de66ce9d7b31cbc3233188471c7260d684df
alexangl <alexangl@mail.ru>
2025-02-04 22:04:56 +0300

    Update Lesson5_2.java

--- src/test/java/Lesson5_2.java
+++ src/test/java/Lesson5_2.java
@@ -8,9 +8,9 @@ import com.codeborne.selenide.DragAndDropOptions;
 
 public class Lesson5_2 {
     @Test
-    void Lesson5_2_Test() {
+    void lesson5_2_Test() {
         System.setProperty("selenide.timeout", "5000");
-        open("/drag_and_drop");
+        open("https://the-internet.herokuapp.com/drag_and_drop/drag_and_drop");
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
         $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
